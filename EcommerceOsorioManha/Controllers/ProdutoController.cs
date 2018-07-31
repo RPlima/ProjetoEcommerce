d6c9dbccbe41e257adeec17dda9a4200df710da6
Estefani Fujimoto <estefani.fujimoto@hotmail.com>
2018-07-31 11:24:20 -0300

    Aula 02 implementação dos métodos Alterar, Excluir.

---
+++ EcommerceOsorioManha/Controllers/ProdutoController.cs
@@ -0,0 +1,82 @@
+﻿using EcommerceOsorioManha.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace EcommerceOsorioManha.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        Context ctx = new Context();
+
+        // GET: Produto
+        public ActionResult Index()
+        {
+            ViewBag.Data = DateTime.Now;
+            ViewBag.Produtos = ctx.Produtos.ToList();
+            return View();
+        }
+
+        public ActionResult CadastrarProduto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CadastrarProduto(string txtNome, string txtDescricao, 
+            string txtPreco, string txtCategoria)
+        {
+            Produto produto = new Produto
+            {
+                Nome = txtNome,
+                Descricao = txtDescricao,
+                Preco = Convert.ToDouble(txtPreco),
+                Categoria = txtCategoria
+            };
+
+            ctx.Produtos.Add(produto);
+            ctx.SaveChanges();
+
+            return RedirectToAction("Index", "Produto");
+        }
+
+        public ActionResult RemoverProduto(int id)
+        {
+            Produto produto = new Produto();
+            produto = ctx.Produtos.Where(x => x.ProdutoId == id).FirstOrDefault();
+            ctx.Produtos.Remove(produto);
+            ctx.SaveChanges();
+            return RedirectToAction("Index", "Produto");
+            //não esquecer de utilizar o RedirectToAction("Nome da página", "Model") pois ao não utiliza-lo gerará uma excessão de arquivo não encontrado
+
+        }
+
+
+        public ActionResult AlterarProduto(int id)
+        {
+            ViewBag.Produto = ctx.Produtos.Where(x => x.ProdutoId == id).FirstOrDefault();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AlterarProduto(int txtid, string txtNome, string txtDescricao, string txtPreco, string txtCategoria)
+        {
+        
+            Produto produto = ctx.Produtos.Where(x => x.ProdutoId == txtid).FirstOrDefault();
+            produto.Nome = txtNome;
+            produto.Descricao = txtDescricao;
+            produto.Preco = Convert.ToDouble(txtPreco);
+            produto.Categoria = txtCategoria;
+
+            // ctx.Entry(produto).State = System.Data.Entity.EntityState.Modified;
+            ctx.Entry(produto).State = EntityState.Modified;
+            ctx.SaveChanges();
+
+            return RedirectToAction("Index", "Produto");
+        }
+
+    }
+}
---
+++ EcommerceOsorioManha/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace EcommerceOsorioManha.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EcommerceOsorioManha.Models.Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(EcommerceOsorioManha.Models.Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
